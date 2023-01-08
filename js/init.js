59e685216366bb1f5e2e39db3daf26920c526de5
Saad Siddiq <siddiq.saad@gmail.com>
2023-01-07 19:04:30 -0700

    lowered fov from 75 to 45

--- js/init.js
+++ js/init.js
@@ -8,8 +8,8 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color('skyblue');
 
 // CAMERA
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.set(7.5, 3, 7.5);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.set(12, 3, 12);
 
 // RENDERER
 const renderer = new THREE.WebGLRenderer({antialias: true});
